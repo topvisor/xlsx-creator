dd3208efe291acea30979df1671f1e368ccaed4e
decaseal <decaseal@gmail.com>
2017-09-08 10:19:44 +0300

    add class StyleXml

---
+++ src/Decaseal/XlsxCreator/Xml/Style/AlignmentXml.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Decaseal\XlsxCreator\Xml\Style;
+
+use Decaseal\XlsxCreator\Xml\BaseXml;
+use XMLWriter;
+
+class AlignmentXml extends BaseXml{
+	function render(XMLWriter $xml, $model = null){
+		if (!$model) return;
+
+		$xml->startElement('alignment');
+
+		if ($model['horizontal'] ?? false) $xml->writeAttribute('horizontal', $model['horizontal']);
+		if ($model['vertical'] ?? false) $xml->writeAttribute('vertical', $model['vertical']);
+		if ($model['wrapText'] ?? false) $xml->writeAttribute('wrapText', 1);
+		if ($model['shrinkToFit'] ?? false) $xml->writeAttribute('shrinkToFit', 1);
+		if ($model['indent'] ?? false) $xml->writeAttribute('indent', (int) $model['indent']);
+
+		if ($model['textRotation'] ?? false) $xml->writeAttribute(
+			'textRotation',
+			$model['textRotation'] === 'vertical' ? 255 : (int) $model['textRotation']
+		);
+
+		switch ($model['readingOrder'] ?? false) {
+			case 'leftToRight': $readingOrder = 1; break;
+			case 'rightToLeft': $readingOrder = 2; break;
+			default: $readingOrder = false; break;
+		}
+
+		if ($readingOrder) $xml->writeAttribute('readingOrder', $readingOrder);
+
+		$xml->endElement();
+	}
+}
---
+++ src/Decaseal/XlsxCreator/Xml/Style/StyleXml.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Decaseal\XlsxCreator\Xml\Style;
+
+use Decaseal\XlsxCreator\Xml\BaseXml;
+use XMLWriter;
+
+class StyleXml extends BaseXml{
+	private $isXfId;
+
+	public function __construct(bool $isXfId = true){
+		$this->isXfId = $isXfId;
+	}
+
+	function render(XMLWriter $xml, $model = null){
+		$model = $model ?? [];
+
+		$xml->startElement('xf');
+
+		$xml->writeAttribute('numFmtId', $model['numFmtId'] ?? 0);
+		$xml->writeAttribute('fontId', $model['fontId'] ?? 0);
+		$xml->writeAttribute('fillId', $model['fillId'] ?? 0);
+		$xml->writeAttribute('borderId', $model['borderId'] ?? 0);
+
+		if ($this->isXfId) $xml->writeAttribute('xfId', $model['xfId'] ?? 0);
+
+		if (isset($model['numFmtId'])) $xml->writeAttribute('applyNumberFormat', 1);
+		if (isset($model['fontId'])) $xml->writeAttribute('applyFont', 1);
+		if (isset($model['fillId'])) $xml->writeAttribute('applyFill', 1);
+		if (isset($model['borderId'])) $xml->writeAttribute('applyBorder', 1);
+
+		if ($model['alignment'] ?? false) {
+			$xml->writeAttribute('applyAlignment', 1);
+			(new AlignmentXml())->render($xml, $model['alignment']);
+		}
+
+		$xml->endElement();
+	}
+}
