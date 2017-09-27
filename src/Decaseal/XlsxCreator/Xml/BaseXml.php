dcdfd4676b4971d2814a234ad0ed953a4bfc3a6f
decaseal <decaseal@gmail.com>
2017-09-27 15:33:21 +0300

    some comments

--- src/Decaseal/XlsxCreator/Xml/BaseXml.php
+++ src/Decaseal/XlsxCreator/Xml/BaseXml.php
@@ -4,10 +4,27 @@ namespace Decaseal\XlsxCreator\Xml;
 
 use XMLWriter;
 
+/**
+ * Class BaseXml. Класс, и все наследующиеся от него, являются оберткой для создания xml кода.
+ *
+ * @package Decaseal\XlsxCreator\Xml
+ */
 abstract class BaseXml{
+	/**
+	 * Создает xml код согласно $model используя XMLWriter $xml
+	 *
+	 * @param XMLWriter $xml - поток для записи xml кода
+	 * @param array|null $model - модель, согласно которой генерируется xml код
+	 */
 	abstract function render(XMLWriter $xml, array $model = null);
 
-	function toXml($model = null) {
+	/**
+	 * Создает xml код согласно $model
+	 *
+	 * @param null $model - модель, согласно которой генерируется xml код
+	 * @return string - xml код
+	 */
+	function toXml($model = null) : string{
 		$xml = new XMLWriter();
 		$xml->openMemory();
 
