63beb053832da17674e749d4fc640bffa338a7c3
decaseal <decaseal@gmail.com>
2017-10-19 11:31:01 +0300

    add class PicPropertiesXml

---
+++ src/Structures/Drawing/PicPropertiesXml.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Topvisor\XlsxCreator\Structures\Drawing;
+
+use Topvisor\XlsxCreator\Xml\BaseXml;
+use XMLWriter;
+
+class PicPropertiesXml extends BaseXml{
+	function render(XMLWriter $xml, array $model = null){
+		$xml->startElement('xdr:nvPicPr');
+
+		$xml->startElement('xdr:cNvPr');
+		$xml->writeAttribute('id', $model['id']);
+		$xml->writeAttribute('name', $model['name']);
+		$xml->endElement();
+
+		$xml->startElement('xdr:cNvPicPr');
+		$xml->startElement('a:picLocks');
+
+		$xml->writeAttribute('noChangeAspect', 1);
+
+		$xml->endElement();
+		$xml->endElement();
+
+		$xml->endElement();
+	}
+}
