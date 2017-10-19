e8215f89d19b74056fc9d1456818d06a0d143bf9
decaseal <decaseal@gmail.com>
2017-10-19 10:34:59 +0300

    add class NoteClientDataXml

---
+++ src/Xml/Comments/Vml/NoteClientDataXml.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Topvisor\XlsxCreator\Xml\Comments\Vml;
+
+use Topvisor\XlsxCreator\Xml\BaseXml;
+use XMLWriter;
+
+class NoteClientDataXml extends BaseXml{
+	function render(XMLWriter $xml, array $model = null){
+		if (!$model) return;
+
+		$xml->startElement('x:ClientData');
+		$xml->writeAttribute('ObjectType', 'Note');
+
+		$xml->writeElement('x:MoveWithCells');
+		$xml->writeElement('x:SizeWithCells');
+		$xml->writeElement('x:Anchor', implode(', ', [
+			$model['col'],
+			15,
+			$model['row'],
+			10,
+			3 + $model['col'] + $model['width'],
+			15,
+			1 + $model['row'] + $model['height'],
+			4
+		]));
+		$xml->writeElement('x:AutoFill', 'False');
+		$xml->writeElement('x:Row', $model['row'] - 1);
+		$xml->writeElement('x:Column', $model['col'] - 1);
+
+		$xml->endElement();
+	}
+}
