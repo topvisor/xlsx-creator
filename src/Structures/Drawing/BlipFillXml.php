a326c9a51b63244fc47f49021cf5c00e3bb2eaff
decaseal <decaseal@gmail.com>
2017-10-19 11:28:55 +0300

    add class BlipFillXml

---
+++ src/Structures/Drawing/BlipFillXml.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Topvisor\XlsxCreator\Structures\Drawing;
+
+use Topvisor\XlsxCreator\Xml\BaseXml;
+use XMLWriter;
+
+class BlipFillXml extends BaseXml{
+	function render(XMLWriter $xml, array $model = null){
+		$xml->startElement('xdr:blipFill');
+
+		$xml->startElement('a:blip');
+		$xml->writeAttribute('xmlns:r', 'http://schemas.openxmlformats.org/officeDocument/2006/relationships');
+		$xml->writeAttribute('r:embed', $model['rId']);
+
+		$xml->startElement('a:extLst');
+
+		$xml->startElement('a:ext');
+		$xml->writeAttribute('uri', '{28A0092B-C50C-407E-A947-70E740481C1C}');
+
+		$xml->startElement('a14:useLocalDpi');
+		$xml->writeAttribute('xmlns:a14', 'http://schemas.microsoft.com/office/drawing/2010/main');
+		$xml->writeAttribute('val', 0);
+		$xml->endElement();
+
+		$xml->endElement();
+		$xml->endElement();
+
+		$xml->endElement();
+
+		$xml->startElement('a:stretch');
+		$xml->writeElement('a:fillRect');
+		$xml->endElement();
+
+		$xml->endElement();
+	}
+}
