b9fd8d8d4dcb15f3f3b76f699b40da025a5f7c38
decaseal <decaseal@gmail.com>
2017-10-19 10:48:04 +0300

    add class CommentXml

---
+++ src/Xml/Comments/CommentXml.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Topvisor\XlsxCreator\Xml\Comments;
+
+use Topvisor\XlsxCreator\Xml\BaseXml;
+use Topvisor\XlsxCreator\Xml\Strings\SharedStringXml;
+use XMLWriter;
+
+class CommentXml extends BaseXml{
+	function render(XMLWriter $xml, array $model = null){
+		$xml->startElement('comment');
+
+		$xml->writeAttribute('authorId', 0);
+		$xml->writeAttribute('ref', $model['address']);
+
+		(new SharedStringXml('text'))->render($xml, $model);
+
+		$xml->endElement();
+	}
+}
