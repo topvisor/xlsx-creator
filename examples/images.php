3f42547ee85b8a6114a67481cb69bd0206136b26
decaseal <decaseal@gmail.com>
2017-10-22 12:35:49 +0300

    images

---
+++ examples/images.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Пример добавления изображений.
+ *
+ * @author decaseal <decaseal@gmail.com>
+ */
+
+use Topvisor\XlsxCreator\Structures\Range\Range;
+use Topvisor\XlsxCreator\Workbook;
+
+include __DIR__.'/../vendor/autoload.php';
+
+$workbook = new Workbook(__DIR__.'/images.xlsx'); // инициализация библиотеки
+$worksheet = $workbook->addWorksheet('Sheet1'); // создание таблицы
+$worksheet->addRow();
+
+$position = new Range(1.5, 1.5, 3.5, 4); // расположение картинки
+$filename = __DIR__.'/images/image'; // путь к картинке
+$extension = 'png'; // расширение картинки
+$name = 'Image'; // имя картинки
+
+$worksheet->addImage($position, $filename, $extension, $name); // добавить картинку
+
+$workbook->commit(); // создание xlsx файла
