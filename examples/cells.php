f191e33a5b8480d12f3fc9eb72b2ed1e21013739
decaseal <decaseal@gmail.com>
2017-10-22 12:15:19 +0300

    cells

---
+++ examples/cells.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Пример использования ячеек.
+ *
+ * @author decaseal <decaseal@gmail.com>
+ */
+
+use Topvisor\XlsxCreator\Workbook;
+
+include __DIR__.'/../vendor/autoload.php';
+
+$workbook = new Workbook(__DIR__.'/cells.xlsx'); // инициализация библиотеки
+$worksheet = $workbook->addWorksheet('Sheet1'); // создание таблицы
+$row = $worksheet->addRow(); // создание строки
+
+$cell = $row->addCell('value'); // добавить ячейку со значением
+$cell = $row->getCell(3); // получить ячейку по номеру столбца
+$cell = $worksheet->getCell(2, 2); // получить ячейку по номеру строки и столбца
+
+$cell->setValue('value'); // значение ячейки
+
+$workbook->commit(); // создание xlsx файла
